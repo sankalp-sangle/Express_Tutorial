d5150727d589b011eeaea874d3c0cbda1bb54c50
Sankalp Sangle <sanku.sangle@gmail.com>
2022-04-01 22:56:51 -0400

    Modified schemas

--- models/food.model.js
+++ models/food.model.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const foodSchema = new Schema({
-    username: { type: String, required: true },
-    ingr: { type: String, required: true },
-    calories: { type: Number, required: true },
+const mealSchema = new Schema({
+    meal_id: { type: Number, required: true, unique: true },
+    meal_name: { type: String, required: true, trim: true },
+    calories: { type: Number, required: true, trim: true },
+    time_of_day: { type: String, required: true, trim: true },
     date: { type: Date, required: true },
+    ingr: { type: String, required: true, trim: true },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User' }
 }, {
     timestamps: true,
 });
 
-const Food = mongoose.model('Food', foodSchema);
+const Meal = mongoose.model('Meal', mealSchema);
 
-module.exports = Food;
+module.exports = Meal;
